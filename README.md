aa9e8451851be4da4984ceeeedec819a071aa529
Lucio <luciocc55@gmail.com>
2022-01-28 11:47:41 -0300

    Init

---
+++ README.md
@@ -0,0 +1,11 @@
+# OpenSwap UI
+
+OpenDao-v1 UI
+
+## Copyright
+
+Attribution-NonCommercial-NoDerivs
+CC BY-NC-ND 
+
+### Preview
+![oswap](https://raw.githubusercontent.com/franbach/oswap-app/main/src/assets/preview/app.gif)
---
+++ src/main.js
@@ -0,0 +1,45 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import TooltipMe from 'tooltip-me'
+import mitt from 'mitt'
+import VueApexCharts from 'vue3-apexcharts'
+
+import Toast from "vue-toastification";
+const options = {
+  transition: "Vue-Toastification__fade",
+  maxToasts: 20,
+  newestOnTop: true
+};
+
+import VueGtag from "vue-gtag-next";
+
+import PerfectScrollbar from 'vue3-perfect-scrollbar'
+import 'vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css'
+
+import "./assets/style/app.scss"
+
+import "@fontsource/alata"
+import "@fontsource/libre-franklin"
+import "@fontsource/nunito"
+import "@fontsource/montserrat"
+
+import 'animate.css';
+
+const app = createApp(App)
+const emitter = mitt();
+app.config.globalProperties.oswapEmit = emitter;
+
+app.use(router);
+app.use(store);
+app.use(PerfectScrollbar);
+app.use(Toast, options);
+app.use(TooltipMe);
+app.use(VueApexCharts);
+app.use(VueGtag, { 
+  property: { 
+    id: "G-HYPSP2E5Q0" 
+  } 
+})
+app.mount('#oswapApp')
